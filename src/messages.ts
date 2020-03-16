56e283ad5109a3f2b38410da45364954ecd45fe3
Kynson <kynsonszetau@icloud.com>
2020-03-16 11:23:13 +0800

    Update project structure.

---
+++ src/messages.ts
@@ -0,0 +1,4 @@
+export const SERVICE_SELECTION_MESSAGE = 'Please select a service:';
+export const PATH_TO_FILE_OR_DIRECTORY_INPUT_MESSAGE = 'Please enter the path to the file/directory you would like Sekreto to process:';
+export const PASSWORD_INPUT_MESSAGE = `Please enter your password:`;
+export const PASSWORD_CONFIRMATION_INPUT_MESSAGE = `Please confirm you password:`;
