de950fba627d21a4e40c076933671726d13a87b0
Kynson <kynsonszetau@icloud.com>
2020-04-14 13:13:06 +0800

    Remove override previous line of logger.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,28 +3,22 @@ import { bold, cyan, red, yellow } from 'chalk';
 type LogSeverity = 'error' | 'info' | 'warning';
 
 interface ILogger {
-  error: (message: string, overridePreviousLine?: boolean) => void;
-  info: (message: string, overridePreviousLine?: boolean) => void;
+  error: (message: string) => void;
+  info: (message: string) => void;
   native: (...message: any[]) => void;
-  warning: (message: string, overridePreviousLine?: boolean) => void;
+  warning: (message: string) => void;
 }
 
-function log(message: string, serverity: LogSeverity, overridePreviousLine: boolean = false) {
+function log(message: string, serverity: LogSeverity) {
   const prefix = serverity === 'error' ? red('✕') : (serverity === 'info' ? cyan('i') : yellow('!'));
   const logMessage = bold(message);
 
-  if (overridePreviousLine) {
-    process.stdout.cursorTo(0);
-    process.stdout.moveCursor(0, -1);
-    process.stdout.clearLine(0);
-  }
-
-  process.stdout.write(`${prefix} ${logMessage}\n`);
+  console.log(`${prefix} ${logMessage}`);
 }
 
 export const logger: ILogger = {
-  error: (message: string, overridePreviousLine?: boolean) => log(message, 'error', overridePreviousLine),
-  info: (message: string, overridePreviousLine?: boolean) => log(message, 'info', overridePreviousLine),
+  error: (message: string) => log(message, 'error'),
+  info: (message: string) => log(message, 'info'),
   native: (...message: any[]) => console.log.call(this, message),
-  warning: (message: string, overridePreviousLine?: boolean) => log(message, 'warning', overridePreviousLine)
+  warning: (message: string) => log(message, 'warning')
 };
